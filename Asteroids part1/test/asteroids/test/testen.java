8d34bd41f77220381f1861ab8670e009b8919c95
Kevin Van der Schueren <kevin.vanderschueren@student.kuleuven.be>
2017-03-11 16:15:11 +0100

    testen methodes 2

--- Asteroids part1/test/asteroids/test/testen.java
+++ Asteroids part1/test/asteroids/test/testen.java
@@ -7,6 +7,29 @@ import asteroids.part1.facade.IFacade;
 
 public class test{
 	
+	public testship(){
+		
+	}
+	
+	public void testposition() {
+		
+	}
+	
+	public void testvelocity(){
+		
+	}
+	
+	public void testmove(){
+		
+	}
+	
+	public void testturn(){
+		
+	}
+	
+	public void testthrust() {
+		
+	}
 	
 	public void testgetDistanceBetween() {
 		
