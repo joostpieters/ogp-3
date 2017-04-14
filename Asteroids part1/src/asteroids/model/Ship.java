ba479973029b53ce39ce4811b9bd73e13eeb2e6f
Kevin Van der Schueren <kevin.vanderschueren@student.kuleuven.be>
2017-04-14 16:43:08 +0200

    firebullet aanpassing

--- Asteroids part1/src/asteroids/model/Ship.java
+++ Asteroids part1/src/asteroids/model/Ship.java
@@ -377,15 +377,30 @@ public class Ship extends CircularObject{
 		
 		double bulletXVelocity = 250*Math.cos(bulletdirection);
 		double bulletYVelocity = 250*Math.sin(bulletdirection);
-		double bulletXPosition = shipPosition[0] + (this.getRadius() + 2*bulletradius)*Math.cos(bulletdirection);
-		double bulletYPosition = shipPosition[1] + (this.getRadius() + 2*bulletradius)*Math.sin(bulletdirection);
+		double bulletXPosition = shipPosition[0] + (this.getRadius() + bulletradius)*Math.cos(bulletdirection);
+		double bulletYPosition = shipPosition[1] + (this.getRadius() + bulletradius)*Math.sin(bulletdirection);
 		
 		firedbullet.setSourceShip(this);
 		this.removeBullet(firedbullet);
 		firedbullet.setVelocity(bulletXVelocity, bulletYVelocity);
 		firedbullet.setPosition(bulletXPosition, bulletYPosition);
-		
+		if ((firedbullet.getPositionArray()[0] + bulletradius > this.getWorld().getWorldDimensionArray()[0]) || 
+				(firedbullet.getPositionArray()[1] + bulletradius > this.getWorld().getWorldDimensionArray()[1]) ||
+				(firedbullet.getPositionArray()[0] + bulletradius < 0) || (firedbullet.getPositionArray()[1] < 0)){
+			firedbullet.terminateBullet();
+		}
+		else{ 
+			for (CircularObject obj: this.getWorld().getAllCircularObjectsInWorld()){
+				if(obj.overlap(firedbullet)){
+					//firedbullet.collisionEntity();
+				}
+				
+			
+			}
+		}
 	}
+	
+	
 	
 	
 	public void terminateShip() {
